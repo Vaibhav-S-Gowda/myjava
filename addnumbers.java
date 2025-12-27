4d26d2e2893bce291e0f562f91fe484bb9e8b526
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2025-12-27 23:37:05 +0530

    Fix formatting and improve user prompts in addnumbers

--- addnumbers.java
+++ addnumbers.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
-public class addnumbers
-{
-public static void main(String[] args)
-{
-Scanner input=new Scanner(System.in);
-System.out.println("enter the first number ");
-int num1=input.nextInt();
-System.out.println("enter the two number ");
-int num2=input.nextInt();
-int sum=num1+num2;
-System.out.println("the sum of "+num1+"and"+num2+"is"+sum);
-}
+
+public class addnumbers {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the first number: ");
+    int num1 = input.nextInt();
+    System.out.println("Enter the second number: ");
+    int num2 = input.nextInt();
+    int sum = num1+num2;
+    System.out.println("The sum of "+num1+" and "+num2+" is "+sum);
+  }
 }
